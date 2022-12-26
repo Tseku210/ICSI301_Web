c366831e09d2933bb260b41cecc07a3fd31c2488
Tseku <otgonbaatartsenguun@yahoo.com>
2022-12-26 23:31:21 +0800

    fixes

--- js/utils/Book.js
+++ js/utils/Book.js
@@ -7,10 +7,10 @@ export class Book {
     <a href="./detail.html">
       <div class="book-img">
         <img src=${this.book.image} alt="ном" />
-        <div class="book-img-btns">
-          <button class="btn circle-btn"><i class="far fa-heart"></i></button>
-          <button class="btn circle-btn"><i class="fas fa-cart-arrow-down"></i></button>
-        </div>
+      </div>
+      <div class="book-img-btns">
+        <button class="btn-book btn-red"><i class="far fa-heart"></i></button>
+        <button class="btn-book btn-green"><i class="fas fa-cart-arrow-down"></i></button>
       </div>
       <div class="tag">
         <p class="book-title">${this.book.title}</p>
