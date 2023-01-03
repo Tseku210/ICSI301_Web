5732fd5fd5ed97b3d848013ae81d8d3e82767630
Tseku <otgonbaatartsenguun@yahoo.com>
2023-01-03 22:52:38 +0800

    za

--- js/utils/Book.js
+++ js/utils/Book.js
@@ -6,11 +6,11 @@ export class Book {
     return `<div>
     <a href="./detail.html">
       <div class="book-img">
-        <img src=${this.book.image} alt="ном" width="140" height="200" />
+        <img src=${this.book.image} alt="ном" width="140">
       </div>
       <div class="book-img-btns">
-        <button class="btn-book btn-red"><i class="far fa-heart"></i></button>
-        <button class="btn-book btn-green"><i class="fas fa-cart-arrow-down"></i></button>
+        <button class="btn-book btn-red" aria-label="btn" aria-labelledby="labelbtn"><i class="far fa-heart"></i></button>
+        <button class="btn-book btn-green" aria-label="btn" aria-labelledby="labelbtn"><i class="fas fa-cart-arrow-down"></i></button>
       </div>
       <div class="tag">
         <p class="book-title">${this.book.title}</p>
